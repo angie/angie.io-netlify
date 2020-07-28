a87f79ed428ec7e52a42d4c0c577e3e3246965d1
Angie Merryweather <6738119+angie@users.noreply.github.com>
2020-07-28 21:25:15 +0100

    update config

--- config/website.js
+++ config/website.js
@@ -1,28 +1,28 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteTitle: 'Angie Merryweather', // Navigation and Site Title
+  siteTitleAlt: "Angie Merryweather's blog", // Alternative Site title for SEO
+  siteTitleShort: 'Angie Merryweather', // short_name for manifest
+  siteUrl: 'https://angie.io', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
+  siteDescription: '',
   author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  organization: 'Buzzing Ltd',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
+  userTwitter: '@amerry', // Twitter Username
   ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: '',
 
   // Manifest and Progress color
-  themeColor: '#5348FF',
+  themeColor: '#e83151',
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
+  twitter: 'https://twitter.com/amerry/',
+  twitterHandle: '@amerry',
+  github: 'https://github.com/angie/',
   linkedin: '',
 }
