cb8c07f9c1a10188a419f89bf86bb0f02558e9b8
Angie Merryweather <6738119+angie@users.noreply.github.com>
2020-07-28 20:54:25 +0100

    switch to dracular syntax theme

--- src/components/mdx/Code.js
+++ src/components/mdx/Code.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import theme from 'prism-react-renderer/themes/oceanicNext'
+import theme from 'prism-react-renderer/themes/dracula'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 
-const Code = ({children, codeString, className = 'language-js', ...props }) => {
+const Code = ({
+  children,
+  codeString,
+  className = 'language-js',
+  ...props
+}) => {
   const language = className.replace(/language-/, '')
   if (props['react-live']) {
     return (
