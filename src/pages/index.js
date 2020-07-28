72b90d8fb7d99f14f4135e6c5bf75147bcf576d2
Angie Merryweather <6738119+angie@users.noreply.github.com>
2020-07-28 21:25:06 +0100

    remove subscribe form

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,8 @@ const Hero = () => {
             max-width: ${rhythm(15)};
           `}
         >
-          Your blog says the things you want to say.
+          Angie Merryweather. Writing about React, Node.js and developer
+          tooling.
         </h1>
       </Container>
       <div
@@ -57,7 +58,7 @@ const Description = styled.p`
 export default function Index({ data: { site, allMdx } }) {
   const theme = useTheme()
   return (
-    <Layout site={site}>
+    <Layout site={site} noSubscribeForm>
       <Hero />
       <Container
         css={css`
